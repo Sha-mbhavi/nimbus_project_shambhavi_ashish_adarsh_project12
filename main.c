db1120338875ec574cc30f1aa935184318e67797
Ashish Yadav <25LBCS3073@culkomail.in>
2025-11-30 22:26:39 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,28 @@
-#incclude<stdio.h>
-#include<stdlib.h>
-#include"parking.h"
-int main(){
-  int ch;
-  setupslot();
-  while(1){
-    printf("\n1.Park 2. remove  3. search  4. show  5. report  6.exit\nchoice:");
-    scanf("%d",&ch);
-    switch(ch){
-      case1:parkcar();
-      break;
-      case2:removecar();
-      break;
-      case3:searchcar();
-      break;
-      case4:showslot();
-      break;
-      case5:showreport();
-      break;
-      case6:
-        free(slots);
-        return 0;
-      default:
-        print("Invalid.\n");
-    }
-  }
-}
+#ifndef PARKING_H
+#define PARKING_H
+
+#include "car.h"
+
+
+struct Slot {
+    int id;           
+    char size[10];  
+    int filled;        
+    struct Car car;  
+};
+
+
+extern struct Slot *slots;
+extern int totalSlots;
+extern float totalMoney;
+
+
+void setupSlots(void);
+void expandSlot(void);
+void parkCar(void);
+void removeCar(void);
+void searchCar(void);
+void showSlots(void);
+void showReport(void);
+
+#endif
